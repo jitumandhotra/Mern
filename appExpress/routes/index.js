1a0fdacb28e2af57096adc2ba891ebfd05116f98
jitumandhotra <jitumandhotra786@gmail.com>
2024-07-01 17:56:33 +0530

    Updated new login, register, hompage features

--- appExpress/routes/index.js
+++ appExpress/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const verifyToken = require('./authMiddleware');
 /* GET home page. */
-router.get('/', verifyToken, function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
--- appExpress/routes/login.js
+++ appExpress/routes/login.js
@@ -14,11 +14,11 @@ router.post('/', async (req, res) => {
     try {
         const user = await User.findOne({ email });
         if (!user) {
-            return res.render('login', { error: 'No User With This Email' });
+            return res.render('login', { notify : { type:'alert-danger', message : 'No User With This Email.'} } );
         }
         const validPassword = await bcrypt.compare(password, user.password);
         if (!validPassword) {
-            return res.render('login', { error: 'Wrong Password' });
+            return res.render('login', { notify : { type:'alert-danger', message : 'Password Do Not Match.'} } );
         }
         const token = jwt.sign({ userId: user._id ,user:user }, process.env.JWT_SECRET);
         res.cookie('auth', token, { httpOnly: true, expires: new Date(Date.now() + 3600000)  });
